e10db6f7c1df87ce5d70e568590d8178dce083d3
flaviocardoso <flavioc401@gmail.com>
2018-06-06 17:35:53 -0300

    inicianando perfil

---
+++ perfil.php
@@ -0,0 +1,18 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta content="width=device-width initial-scale=1 maximum-scale=1" name="viewport">
+    <tile>Perfil</title>
+  </head>
+  <body>
+    <header>
+    </header>
+    <section>
+    </section>
+  </body>
+</html>
