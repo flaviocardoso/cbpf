bf6dbac55af4464ebacbcac143439eebd695a32a
flaviocardoso <flavioc401@gmail.com>
2018-06-28 16:53:39 -0300

    teste comentado

--- coord_orderservice.php
+++ coord_orderservice.php
@@ -4,7 +4,7 @@ include("biblio.php");
 
 session_start();
 $coord = $_SESSION["coord"];
-echo $coord;
+//echo $coord;
 $sql1 = "SELECT os.idos as id, os.nos as nos, os.nome as solicitante, os.descr as descr, os.setor as setor, date_format(os.datahora, '%d/%m/%Y') as data, TIME(os.datahora) as hora, te.nome as tecnico, date_format(te.datahora, '%d/%m/%Y') as data_ultima, TIME(te.datahora) as hora_ultima, te.status as status, te.laudo as laudo FROM orderservice os JOIN (SELECT idos, nome, user, setor, datahora, status, laudo FROM tecnico group by idos desc) te using(idos) WHERE os.coord=:coord";
 $stmt1 = $PDO->prepare($sql1);
 $stmt1->bindParam(":coord", $coord, PDO::PARAM_STR);
