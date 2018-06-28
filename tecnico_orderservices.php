3117e4cf9df221464d6489b57d1820636f2defc2
flaviocardoso <flavioc401@gmail.com>
2018-06-28 15:47:24 -0300

    using não funcionado para duas variaveis - mudança para where

--- tecnico_orderservices.php
+++ tecnico_orderservices.php
@@ -5,11 +5,13 @@ include("biblio.php");
 //fazer query para pergar user e setor para mostrar as ordem de serviços alteradas por usuario
 session_start();
 $user = $_SESSION["user"];
-//echo $user;
-$sql1 = "SELECT os.idos as id, os.nos as nos, os.nome as solicitante, os.descr as descr, os.setor as setor, date_format(os.datahora, '%d/%m/%Y') as data, TIME(os.datahora) as hora, te.nome as tecnico, date_format(te.datahora, '%d/%m/%Y') as data_ultima, TIME(te.datahora) as hora_ultima, te.status as status, te.laudo as laudo FROM orderservice os JOIN (SELECT idos, nome, user, setor, datahora, status, laudo FROM tecnico group by idos desc) te using(idos) where te.user=:user";
+$setor = $_SESSION["setor"];
+//echo $setor;
+$sql1 = "SELECT os.idos as id, os.nos as nos, os.nome as solicitante, os.descr as descr, os.setor as setor, date_format(os.datahora, '%d/%m/%Y') as data, TIME(os.datahora) as hora, te.nome as tecnico, date_format(te.datahora, '%d/%m/%Y') as data_ultima, TIME(te.datahora) as hora_ultima, te.status as status, te.laudo as laudo FROM orderservice os JOIN (SELECT idos, nome, user, setor, datahora, status, laudo FROM tecnico group by idos desc) te using(idos) WHERE te.user=:user and te.setor=:setor";
 
 $stmt1 = $PDO->prepare($sql1);
 $stmt1->bindParam(":user", $user, PDO::PARAM_STR);
+$stmt1->bindParam(":setor", $setor, PDO::PARAM_STR);
 $stmt1->execute();
 $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
 //print_r($rows1);
