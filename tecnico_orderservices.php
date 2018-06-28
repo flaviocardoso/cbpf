dfa59abd29fec6ab832300c21cac58a3ec9dcff7
flaviocardoso <flavioc401@gmail.com>
2018-06-28 14:47:15 -0300

    inserindo a consulta - teste

--- tecnico_orderservices.php
+++ tecnico_orderservices.php
@@ -3,6 +3,15 @@ include("conexao.php");
 include("biblio.php");
 
 //fazer query para pergar user e setor para mostrar as ordem de serviços alteradas por usuario
+session_start();
+$user = $_SESSION("user");
+
+$sql1 = "SELECT os.idos as id, os.nos as nos, os.nome as solicitante, os.descr as descr, os.setor as setor, date_format(os.datahora, '%d/%m/%Y') as data, TIME(os.datahora) as hora, te.nome as tecnico, date_format(te.datahora, '%d/%m/%Y') as data_ultima, TIME(te.datahora) as hora_ultima, te.status as status, te.laudo as laudo FROM orderservice os JOIN (SELECT idos, nome, setor, datahora, status, laudo FROM tecnico group by idos desc) te using(idos) where te.user=:user";
+
+$stmt1 = $PDO->prepare($sql1);
+$stmt1->bindParam(":user", $user, PDO::PARAM_STR);
+$stmt1->execute();
+$rows1 = $stmt1->fetchAll(PDP::FETCH_ASSOC);
 
 ?>
 <!DOCTYPE html>
