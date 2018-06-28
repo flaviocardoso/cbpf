937b728e22cef801f38badc066e3a01c2ad7bdf0
flaviocardoso <flavioc401@gmail.com>
2018-06-28 15:13:16 -0300

    correção em sql e consultas de variaveis

--- tecnico_orderservices.php
+++ tecnico_orderservices.php
@@ -5,13 +5,14 @@ include("biblio.php");
 //fazer query para pergar user e setor para mostrar as ordem de serviços alteradas por usuario
 session_start();
 $user = $_SESSION["user"];
-
-$sql1 = "SELECT os.idos as id, os.nos as nos, os.nome as solicitante, os.descr as descr, os.setor as setor, date_format(os.datahora, '%d/%m/%Y') as data, TIME(os.datahora) as hora, te.nome as tecnico, date_format(te.datahora, '%d/%m/%Y') as data_ultima, TIME(te.datahora) as hora_ultima, te.status as status, te.laudo as laudo FROM orderservice os JOIN (SELECT idos, nome, setor, datahora, status, laudo FROM tecnico group by idos desc) te using(idos) where te.user=:user";
+//echo $user;
+$sql1 = "SELECT os.idos as id, os.nos as nos, os.nome as solicitante, os.descr as descr, os.setor as setor, date_format(os.datahora, '%d/%m/%Y') as data, TIME(os.datahora) as hora, te.nome as tecnico, date_format(te.datahora, '%d/%m/%Y') as data_ultima, TIME(te.datahora) as hora_ultima, te.status as status, te.laudo as laudo FROM orderservice os JOIN (SELECT idos, nome, user, setor, datahora, status, laudo FROM tecnico group by idos desc) te using(idos) where te.user=:user";
 
 $stmt1 = $PDO->prepare($sql1);
 $stmt1->bindParam(":user", $user, PDO::PARAM_STR);
 $stmt1->execute();
 $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
+//print_r($rows1);
 
 ?>
 <!DOCTYPE html>
@@ -31,8 +32,11 @@ $rows1 = $stmt1->fetchAll(PDO::FETCH_ASSOC);
         <?php
           foreach ($rows1 as $key => $value) {
             echo "$value[descr]<br>";
+            echo "$value[data]<br>";
+            echo "$value[hora]<br>";
             echo "$value[laudo]<br>";
-            echo "$value[dhultima]<br>";
+            echo "$value[data_ultima]<br>";
+            echo "$value[hora_ultima]<br>";
             echo "<a href=\"ver_os.php?id=$value[id]\" target=\"_blank\">Acessar</a><br>";
             // code...
           }
