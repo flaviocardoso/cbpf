b158615efb9211c9dd03ac4bc2b5dc4226e324eb
flaviocardoso <flavioc401@gmail.com>
2018-07-04 20:27:53 -0300

    ainda não encotrado o erro

--- criar_orderservice.php
+++ criar_orderservice.php
@@ -9,14 +9,99 @@ if (!isset($_SESSION['user'])) {
     // Redireciona o visitante de volta pro login
     header("Location: login.php"); exit;
 }
-include("biblio.php");
-include("conexao.php");
+$mensErroArq = "";
+$mensErro = "";
+$mensOS = "";
+echo "string1";
+//post isset não funcionando!!!
+if(isset($_POST["submit"]))
+{
+  echo "string2";
+  $nome = entrada($_POST["nome"]);
+  $email = entrada($_POST["email"]);
+  $coord = entrada($_POST["coord"]);
+  $ala = entrada($_POST["ala"]);
+  $sala = entrada($_POST["sala"]);
+  $telefone = entrada($_POST["telefone"]);
+  $setor = entrada($_POST["setor"]);
+  $descr = entrada($_POST["descr"]);
+
+  $dt = new DateTime('now', new DateTimeZone('America/Sao_Paulo'));
+  $nos = $coord."/".$dt->format('YmdHis');
+  $dh = $dt->format('Y-m-d H:i:s');
+
+  if(!empty($_FILES['arq']))
+  {
+  $path = '/opt/lampp/htdocs/cbpf/conec/data/uploads/';
+  //$dt = new DateTime('now', new DateTimeZone('America/Sao_Paulo'));
+  $forderdh = $dt->format('Y/m/d/H/i/s/');
+
+  $path = $path . $forderdh;
+  $arq_name = $path . $_FILES['arq']['name'];
+  $_SESSION["arq_name"] = $arq_name;
+  $_SESSION["forders"] = $forderdh;
+
+  }
+  else
+  {
+    $mensErroArq = "<p>Erro de Arquivo</p>";
+  }
+  include("config/maining/path/CN.php");
+  $PDO = new CN();
+  $rwci1 = $PDO->inserirOS($user, $nos, $nome, $email, $coord, $ala, $sala, $telefone, $setor, $arq, $dh, $descr);
+  $rowsi1 = $rwci1[0];
+  $counti1 = $rwci1[1];
+
+  if($counti1 > 0)
+  {
+    $rwci2 = $PDO->orderServicePorNOS($nos);
+    $rowsi2 = $rwci2[0];
+    $counti2 = $rwci2[1];
+
+    if($counti2 > 0)
+    {
+      $status = "NOVA";
+      $rwci3 = $PDO->inserirTecnNOVAOS($rowsi2["id"], $setor, $dh, $status);
+      $counti3 = $rwci3[0];
+      if($counti3 > 0){
+        header("Location: conec/data/uploads.php");
+        $mensOS = "Ordem de Serviço Criada!";
+      }else{
+        $mensErro = "<p>rwci3 erro!</p>";
+      }
+
+    }else{
+      $mensErro = "<p>rwci2 erro!</p>";
+    }
+  }else{
+    $mensErro = "<p>rwci1 erro</p>";
+  }
+}
+
+include_once("config/maining/path/biblio.php");
+$mens = "";
+$rows1 = array();
+$count = 0;
+if(isset($user) and !empty($user))
+{
+  include_once("config/maining/path/CN.php");
+  $PDO = new CN();
+  $rwc = $PDO->consultaUser($user);
+  $rows1 = $rwc[0];
+  $count = $rwc[1];
+  if($count == 0)
+  {
+    $mens = "<p>Não encontrado</p>";
+  }
+}
+/*
 $sql = "select id, nome, email, telefone, setor, sala, coord, ala from usuario where user=:user";
 $stmt = $PDO->prepare($sql);
 $stmt->bindParam(":user", $user. PDO::PARAM_STR);
 $stmt->execute();
-
+$count = $stmt->rowCount();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
+*/
 ?>
 
 <!--<!DOCTYPE html>
@@ -32,7 +117,11 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
       <p>Criando a ordem de serviço</p>
     </header>
     <section>-->
-      <form id="form_os" method="post">
+    <? echo $mensErroArq; ?>
+    <? echo $mensErro; ?>
+    <? echo $mensOS; ?>
+    <? echo $mens; ?>
+      <form id="form_os" method="post" action="principal#!/criarOS" enctype="multipart/form-data">
         <fieldset>
           <legend>Criando Ordem de Serviço</legend>
           <!--<fieldset>
@@ -44,27 +133,27 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
             <legend>Dados do solicitante</legend>
             <fieldset>
               <legend>Solicitante</legend>
-              <input id="nome" type="text" name="nome" value="<?php echo $row["nome"]; ?>" required/>
+              <input id="nome" type="text" name="nome" value="<?php echo $rows1["nome"]; ?>" required/>
             </fieldset>
             <fieldset>
               <legend>Email</legend>
-              <input id="email"  type="text" name="email" value="<?php echo $row["email"]; ?>" required/>
+              <input id="email"  type="text" name="email" value="<?php echo $rows1["email"]; ?>" required/>
             </fieldset>
             <fieldset>
               <legend>Coordenação</legend>
-              <input id="coord" type="text" name="coord" value="<?php echo $row["coord"]; ?>" required/>
+              <input id="coord" type="text" name="coord" value="<?php echo $rows1["coord"]; ?>" required/>
             </fieldset>
             <fieldset>
               <legend>Ala</legend>
-              <input id="ala" type="text" name="ala" value="<?php echo $row["ala"]; ?>" required/>
+              <input id="ala" type="text" name="ala" value="<?php echo $rows1["ala"]; ?>" required/>
             </fieldset>
             <fieldset>
               <legend>Sala</legend>
-              <input id="sala" type="text" name="sala" value="<?php echo $row["sala"]; ?>" required/>
+              <input id="sala" type="text" name="sala" value="<?php echo $rows1["sala"]; ?>" required/>
             </fieldset>
             <fieldset>
               <legend>Telefone</legend>
-              <input id="telefone" type="tel" name="telefone" value="<?php echo $row['telefone']; ?>" required/>
+              <input id="telefone" type="tel" name="telefone" value="<?php echo $rows1['telefone']; ?>" required/>
             </fieldset>
             <fieldset>
               <legend>Setor de destinação</legend>
@@ -81,13 +170,13 @@ $row = $stmt->fetch(PDO::FETCH_ASSOC);
           </fieldset>
           <fieldset>
               <legend>Arquivos</legend>
-              <input type="file" name="arquivo[]" multiple/>
+              <input type="file" name="arq"/>
           </fieldset>
           <br/>
           <fieldset>
             <legend>Descrição do serviço</legend>
             <textarea id="descr" name="descr" cols="30" rows="10" form="form_os"></textarea><br/><br/>
-            <button type="submit" formaction="enviar_orderservice.php">Confirmar</button>
+            <input type="submit" name="submit"/>Confirmar</button>
             <!--<button type="submit" formaction="cancelar_orderservice.php">Cancelar</button>-->
           </fieldset>
         </fieldset>
