4b42fcc230cb745bc7b7a7d23d8e66b877a2b57d
flaviocardoso <flavioc401@gmail.com>
2018-07-04 00:48:50 -0300

    teste para adicionar vários arquivo -> idéia descartada agora

---
+++ testes/teste_multfile.php
@@ -0,0 +1,4 @@
+<form enctype="multipart/form-data" method="post" action="/cbpf/conec/data/uploads/config_uploads.php">
+  Adicone arquivos : <input type="file" name="arquivo[]" multiple/>
+  <input type="submit" name="submit"/>
+</form>
