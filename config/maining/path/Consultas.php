c9bf29b4480e4e02ffbbe218dfe6335de8ce5ae1
flaviocardoso <flavioc401@gmail.com>
2018-07-12 17:12:56 -0300

    criação da classe consultas

---
+++ config/maining/path/Consultas.php
@@ -0,0 +1,10 @@
+<?
+/**
+ *
+ */
+class ClassConsulta
+{
+  public $nos, $status, $coord, $setor, $data, $datafinal, $anoinicial, $mesinicial, $diainicial, $anofinal, $mesfinal, $diafinal, $tecnico, $solicitante; 
+}
+
+?>
